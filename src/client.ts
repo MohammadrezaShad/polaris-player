b4f4ccc14bffc50a24ca13d2509b48b571cb3da0
shadmobaraki <m.shadmobaraki@gmail.com>
2025-09-22 12:42:10 +0330

    wip : remove client export

--- src/client.ts
+++
@@ -1,2 +0,0 @@
-"use client"; // Next.js sees this in source
-export { default as VideoPlayer } from "./polaris/ui/video-player-v2";
--- src/polaris/index.ts
+++ src/polaris/index.ts
@@ -1,5 +1,7 @@
+/** src/player/index.ts */
 export * from "./ports";
 export * from "./core/events";
 export * from "./core/state";
 export * from "./core/machine";
 export * from "./providers/player-provider";
+export { default as VideoPlayer } from "./ui/video-player-v2";
